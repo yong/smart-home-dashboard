7cd21d525b513611ef0b47c8ce2ce06707d07a9f
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-03-20 14:22:27 -0400

    implement Header component with react-native-vector-icons

---
+++ Header.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Header = () => (
+  <View style={styles.header}>
+    <Icon name="shield" size={30} color="#fff" />
+    <View style={styles.rightIcons}>
+      <Icon name="globe" size={30} color="#00ff00" />
+      <Icon name="bell" size={30} color="#fff" />
+      <Icon name="video-camera" size={30} color="#fff" />
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#000',
+    padding: 10,
+  },
+  rightIcons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: 100, // Adjust this value as needed
+  },
+});
+
+export default Header;
