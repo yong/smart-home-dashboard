ad84fb03b59b9796be8362952d2fa017fbcf41a7
Xue Yong Zhi <xue.yong.zhi@gmail.com>
2024-03-23 13:38:20 -0400

    add SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -8,29 +9,31 @@ const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color, size }) => {
-            return <Icon name={route.name} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'white',
-          tabBarInactiveTintColor: 'gray',
-          tabBarStyle: {
-            backgroundColor: '#333',
-          },
-          tabBarLabelStyle: {
-            display: 'none',
-          },
-        })}
-      >
-        <Tab.Screen name="windows" component={Home} options={{ headerShown: false }} />
-        <Tab.Screen name="server" component={Home} options={{ headerShown: false }} />
-        <Tab.Screen name="home" component={Home} options={{ headerShown: false }} />
-        <Tab.Screen name="music" component={Home} options={{ headerShown: false }} />
-        <Tab.Screen name="bars" component={Home} options={{ headerShown: false }} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              return <Icon name={route.name} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'white',
+            tabBarInactiveTintColor: 'gray',
+            tabBarStyle: {
+              backgroundColor: '#333',
+            },
+            tabBarLabelStyle: {
+              display: 'none',
+            },
+          })}
+        >
+          <Tab.Screen name="windows" component={Home} options={{ headerShown: false }} />
+          <Tab.Screen name="server" component={Home} options={{ headerShown: false }} />
+          <Tab.Screen name="home" component={Home} options={{ headerShown: false }} />
+          <Tab.Screen name="music" component={Home} options={{ headerShown: false }} />
+          <Tab.Screen name="bars" component={Home} options={{ headerShown: false }} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 };
 
